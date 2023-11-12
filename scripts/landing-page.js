6784e33e38bded08ff1150969c0eeddee90d71a0
Felipe <felipepeengler@outlook.com>
2023-11-12 18:36:56 -0300

    fix landing page

--- scripts/landing-page.js
+++ scripts/landing-page.js
@@ -43,8 +43,6 @@ contactsBtn.addEventListener('click', () => {
 });
 
 openModalBtn.addEventListener('click', () => {
-    window.scrollTo({behavior: 'smooth', top: 0});
-
     modalContainer.classList.remove('hidden');
     overlay.classList.remove('hidden');
 
