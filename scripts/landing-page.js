64c64cf92ac1a8db4c8e35c736a7914165563f58
Felipe <felipepeengler@outlook.com>
2023-11-12 16:00:36 -0300

    minor fix and login dialog

--- scripts/landing-page.js
+++ scripts/landing-page.js
@@ -7,7 +7,20 @@ const contactsBtn = document.querySelector('#contacts-btn');
 const modalContainer = document.querySelector('#modal-container');
 const overlay = document.querySelector('.overlay');
 const openModalBtn = document.querySelector('#login-btn');
-const closeModalBtn = document.querySelector('#close-modal-btn');
+const closeModalBtn = document.querySelector('#close-btn');
+
+const forgotPasswordBtn = document.querySelector('#forgot-password');
+const middleOne = document.querySelector('#middle-one');
+const bottomOne = document.querySelector('#bottom-one');
+const middleTwo = document.querySelector('#middle-two');
+const bottomTwo = document.querySelector('#bottom-two');
+const middleThree = document.querySelector('#middle-three');
+const bottomThree = document.querySelector('#bottom-three');
+
+const fpBackBtn = document.querySelector('#fp-back-btn');
+const submitFpBtn = document.querySelector('#submit-fp-btn');
+
+const fpSuccessBackBtn = document.querySelector('#fp-success-back-btn');
 
 logoBtn.addEventListener('click', () => {
     window.scrollTo({behavior: 'smooth', top: 0});
@@ -32,9 +45,46 @@ contactsBtn.addEventListener('click', () => {
 openModalBtn.addEventListener('click', () => {
     modalContainer.classList.remove('hidden');
     overlay.classList.remove('hidden');
+
+    middleOne.classList.remove('hidden');
+    middleTwo.classList.add('hidden');
+    middleThree.classList.add('hidden');
+
+    bottomOne.classList.remove('hidden');
+    bottomTwo.classList.add('hidden');
+    bottomThree.classList.add('hidden');
 });
 
 closeModalBtn.addEventListener('click', () => {
     modalContainer.classList.add('hidden');
     overlay.classList.add('hidden');
 });
+
+forgotPasswordBtn.addEventListener('click', () => {
+    middleOne.classList.add('hidden');
+    middleTwo.classList.remove('hidden');
+    bottomOne.classList.add('hidden');
+    bottomTwo.classList.remove('hidden');
+});
+
+fpBackBtn.addEventListener('click', () => {
+    middleTwo.classList.add('hidden');
+    middleOne.classList.remove('hidden');
+    bottomTwo.classList.add('hidden');
+    bottomOne.classList.remove('hidden');
+});
+
+submitFpBtn.addEventListener('click', () => {
+    middleTwo.classList.add('hidden');
+    middleThree.classList.remove('hidden');
+    bottomTwo.classList.add('hidden');
+    bottomThree.classList.remove('hidden');
+});
+
+fpSuccessBackBtn.addEventListener('click', () => {
+    middleThree.classList.add('hidden');
+    middleOne.classList.remove('hidden');
+    bottomThree.classList.add('hidden');
+    bottomOne.classList.remove('hidden');
+});
+
