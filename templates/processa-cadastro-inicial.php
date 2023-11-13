83aa5cdc0c2e5341768788275007e1b5998ae646
Leonardo <leoklein_pr@hotmail.com>
2023-11-13 19:01:40 -0300

    correcao

--- templates/processa-cadastro-inicial.php
+++ templates/processa-cadastro-inicial.php
@@ -4,7 +4,7 @@
         $email = $_POST['email'];
         $senha = md5($_POST['senha']);
 
-        $conn->query("INSERT INTO tbpessoa (nome, email, senha) VALUES (NULL,'$email', '$senha')");
+        $conn->query("INSERT INTO tbpessoa (email, senha) VALUES ('$email', '$senha')");
 
         header('location: admin.php');
     }
